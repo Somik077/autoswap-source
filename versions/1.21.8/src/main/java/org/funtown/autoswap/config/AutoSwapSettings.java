47b97abca515af82cf6b8033ad7eb403980b2fcd
Somik <vavanvova35@gmail.com>
2026-05-31 21:41:59 +0400

    Deleted folder

--- versions/1.21.8/src/main/java/org/funtown/autoswap/config/AutoSwapSettings.java
+++
@@ -1,8 +0,0 @@
-package org.funtown.autoswap.config;
-
-public class AutoSwapSettings {
-    public int     inventoryOpenDelayTicks = 1;
-    public int     swapCooldownMs          = 300;
-    public boolean showActionBar           = true;
-    public String  language                = "en_us";
-}
